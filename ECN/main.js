2e4385cac5b22e5f8cc69ab01d79aed47b5162a7
claudiojpg <claudiovalencia13@gmail.com>
2023-04-30 15:10:12 -0400

    ECN 1.2

--- ECN/main.js
+++ ECN/main.js
@@ -33,3 +33,11 @@ time.innerHTML= local.toLocaleTimeString();
 date.innerHTML=`${day} ${monthNames[month]} ${year}`;
 
 },1000);
+
+const passwordInput = document.querySelector("#password")
+const eye = document.querySelector("#eye")
+eye.addEventListener("click", function(){
+    this.classList.toggle("fa-eye-slash")
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+    passwordInput.setAttribute("type", type)
+  })
